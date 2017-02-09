c7d2aefaa525a83aa40f26ddfe9274877019d050
Ivan Tovalin <ivantovalin12@gmail.com>
2017-02-09 10:18:02 -0800

    Final commit :(
    its not the way  i wanted it to turn out to be

--- random.js
+++ random.js
@@ -5,9 +5,10 @@ var namespace = "http://www.w3.org/2000/svg"
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createFirstScene() {
+  var number = Math.random()
+  if(number < 0.5){
   makeRect(0,0,200,200, "#1B2B2F", 1)
   makeCircle(100, 50, 25, "#3380B5", 1)
-  makeCircle(150,30 , 10, "white" ,1)
   makeCircle(150,30 , 10, "#778471" ,1)
   makeRect(85,50,20,10,"#399C0E", 1)
   makeCircle(85,55,5,"#399C0E",1 )
@@ -21,8 +22,6 @@ function createFirstScene() {
   makeRect(100,62,15,6,"#399C0E",1)
   makeCircle(100,65,3,"#399C0E", 1)
   makeCircle(115,65,3,"#399C0E",1)
-
-
   makeCircle(130,28,1,"white",1)
   makeCircle(120,21,2,"white",1)
   makeCircle(120,21,2,"white",1)
@@ -37,23 +36,64 @@ function createFirstScene() {
   makeCircle(150,76,4,"white",1)
   makeCircle(153,68,1,"white",1)
   makeCircle(139,76,2,"white",1)
+  makeLine(25, 25, 50, 50, "aqua", 2, 1)
+  
+} else {
+  makeCircle(50,30 , 10, "#778471" ,1)
+  makeRect(0,0,200,200, "#1B2B2F", 1)
+  makeCircle(100, 50, 25, "#3380B5", 1)
+  makeRect(85,50,20,10,"#399C0E", 1)
+  makeCircle(85,55,5,"#399C0E",1 )
+  makeCircle(105,55,5, "#399C0E", 1)
+  makeRect(95,35,20,10,"#399C0E")
+  makeCircle(95,40,5,"#399C0E", 1)
+  makeCircle(114,40,5,"#399C0E", 1)
+  makeRect(95,33,20,10,"#399C0E")
+  makeCircle(95,38,5,"#399C0E",1)
+  makeCircle(114,38,5,"#399C0E", 1)
+  makeRect(100,62,15,6,"#399C0E",1)
+  makeCircle(100,65,3,"#399C0E", 1)
+  makeCircle(115,65,3,"#399C0E",1)
+  makeCircle(130,28,1,"white",1)
+  makeCircle(120,21,2,"white",1)
+  makeCircle(120,21,2,"white",1)
+  makeCircle(38,47,4,"white",1)
+  makeCircle(18,17,2,"white",1)
+  makeCircle(37,27,4,"white",1)
+  makeCircle(135,49,3,"white",1)
+  makeCircle(65,81,2,"white",1)
+  makeCircle(24,59,3,"white",1)
+  makeCircle(65,48,1,"white",1)
+  makeCircle(54,37,2,"white",1)
+  makeCircle(150,76,4,"white",1)
+  makeCircle(153,68,1,"white",1)
+  makeCircle(139,76,2,"white",1)
+  makeCircle(50,30,10,"#778471",1)
 }
-
+}
 // Fill in this function so that it draws something using SVG shapes!
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createSecondScene() {
+  var number= Math.random()
+  if(number<0.5){
     makeRect(0,0,200,200, "black", 1)
-    makeCircle(100, 50, 25, "#e05135", 0.9)
+    makeLine(150, 50, 50, 50, "aqua", 2, 0.7)
+    makeCircle(100, 50, 25, "#e05135", 1)
+}else{
+  makeCircle(160,40,10,"#778471",1)
 }
-
+}
 // Fill in this function so that it draws something using SVG shapes!
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createThirdScene() {
   makeRect(0,0,200,200, "black", 1)
-  makeCircle(100, 50, 25, "#9D4109", 0.9)
+  makeCircle(100, 50, 25, "#9D4109", 1)
+  makeLine(150, 50, 50, 50, "brown", 2, 0.9)
+
 }
+
 
 
 
